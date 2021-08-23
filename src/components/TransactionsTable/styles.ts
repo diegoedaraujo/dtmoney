655e976039f45f5ab821c695b92662b3b90e6511
Diego Araújo <diegoedaraujo@gmail.com>
2021-08-23 15:13:26 -0300

    Estilizando

--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -18,7 +18,20 @@ export const Container = styled.div`
       padding: 1rem 2rem;
       border: 0;
       background: var(--shape);
-      font-weight: 400;
+      color: var(--text-body);
+      border-radius: 0.25rem;
+
+      &:first-child {
+        color: var(--text-title);
+      }
+
+      &.deposit {
+        color: var(--green);
+      }
+
+      &.withdraw {
+        color: var(--red);
+      }
     }
   }
 `;
